27b3c024f21cf639932f7a2b858a463a3804538f
MIXVBRC <mixvbrc@gmail.com>
2022-08-19 03:53:26 +1000

    mda

--- local/templates/mda/components/bitrix/news/.default/bitrix/news.list/.default/template.php
+++ local/templates/mda/components/bitrix/news/.default/bitrix/news.list/.default/template.php
@@ -19,14 +19,13 @@ $this->setFrameMode(true);
 
                 <?foreach($arResult["ITEMS"] as $arItem):?>
                 <?
-                pre($arItem);
                     $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
                     $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
                 ?>
 
                 <a class="list2__item" href="<?=$arItem['DETAIL_PAGE_URL']?>" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
                     <div class="list2__img">
-                        <img class="img__cover" src="img/element.jpeg" alt="">
+                        <img class="img__cover" src="<?=$arItem['PREVIEW_PICTURE']['SRC']?>" alt="<?=$arItem['PREVIEW_PICTURE']['ALT']?>">
                     </div>
                     <div class="list2__info">
                         <div class="list2__name"><?=$arItem['NAME']?></div>
