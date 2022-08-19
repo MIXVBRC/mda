3145ce705fef04f43b2d3f7a7a79a5d9cf469752
MIXVBRC <mixvbrc@gmail.com>
2022-08-19 21:38:59 +1000

    mda

--- local/templates/mda/components/bitrix/catalog/.default/section.php
+++ local/templates/mda/components/bitrix/catalog/.default/section.php
@@ -70,7 +70,6 @@ if ($isFilter)
 		$arCurSection = array();
 }
 ?>
-<div class="row">
 <?
 
 if (isset($arParams['USE_COMMON_SETTINGS_BASKET_POPUP']) && $arParams['USE_COMMON_SETTINGS_BASKET_POPUP'] === 'Y')
@@ -224,9 +223,38 @@ $APPLICATION->IncludeComponent(
     ),
     $component
 );
-if ($isVerticalFilter)
-	include($_SERVER["DOCUMENT_ROOT"]."/".$this->GetFolder()."/section_vertical.php");
-else
-	include($_SERVER["DOCUMENT_ROOT"]."/".$this->GetFolder()."/section_horizontal.php");
+
+$sectionListParams = array(
+    "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+    "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+    "SECTION_ID" => $arResult["VARIABLES"]["SECTION_ID"],
+    "SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
+    "CACHE_TYPE" => $arParams["CACHE_TYPE"],
+    "CACHE_TIME" => $arParams["CACHE_TIME"],
+    "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+    "COUNT_ELEMENTS" => $arParams["SECTION_COUNT_ELEMENTS"],
+    "TOP_DEPTH" => $arParams["SECTION_TOP_DEPTH"],
+    "SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
+    "VIEW_MODE" => $arParams["SECTIONS_VIEW_MODE"],
+    "SHOW_PARENT_NAME" => $arParams["SECTIONS_SHOW_PARENT_NAME"],
+    "HIDE_SECTION_NAME" => (isset($arParams["SECTIONS_HIDE_SECTION_NAME"]) ? $arParams["SECTIONS_HIDE_SECTION_NAME"] : "N"),
+    "ADD_SECTIONS_CHAIN" => (isset($arParams["ADD_SECTIONS_CHAIN"]) ? $arParams["ADD_SECTIONS_CHAIN"] : '')
+);
+
+if ($sectionListParams["COUNT_ELEMENTS"] === "Y")
+{
+    $sectionListParams["COUNT_ELEMENTS_FILTER"] = "CNT_ACTIVE";
+    if ($arParams["HIDE_NOT_AVAILABLE"] == "Y")
+    {
+        $sectionListParams["COUNT_ELEMENTS_FILTER"] = "CNT_AVAILABLE";
+    }
+}
+$APPLICATION->IncludeComponent(
+    "bitrix:catalog.section.list",
+    "",
+    $sectionListParams,
+    $component,
+    array("HIDE_ICONS" => "Y")
+);
+unset($sectionListParams);
 ?>
-</div>
