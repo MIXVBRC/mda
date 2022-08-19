f5736f8ce0f9dcc902c05ab4e970831dc4c74aef
MIXVBRC <mixvbrc@gmail.com>
2022-08-19 22:18:58 +1000

    mda

--- local/templates/mda/components/bitrix/catalog/.default/bitrix/catalog.section.list/.default/template.php
+++ local/templates/mda/components/bitrix/catalog/.default/bitrix/catalog.section.list/.default/template.php
@@ -10,7 +10,7 @@
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
-$this->setFrameMode(true);
+$this->setFrameMode(false);
 
 $strSectionEdit = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "SECTION_EDIT");
 $strSectionDelete = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "SECTION_DELETE");
@@ -24,12 +24,12 @@ if ($arResult["SECTIONS_COUNT"] > 0):?>
             <div class="sections-mini__list">
 
                 <?foreach ($arResult['SECTIONS'] as $arSection):?>
-                <?
+                    <?
                     if ($arSection['ELEMENT_CNT'] <= 0) continue;
 
                     $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
                     $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
-                ?>
+                    ?>
 
                     <div class="sections-mini__item" id="<?=$this->GetEditAreaId($arSection['ID'])?>">
                         <a href="<?=$arSection['SECTION_PAGE_URL']?>" class="sections-mini__link"><?=$arSection['NAME']?></a>
