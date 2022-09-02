c5d9801bad99a6cb81d960acef8642512ebc97d9
MIXVBRC <mixvbrc@gmail.com>
2022-09-03 03:21:57 +1000

    mda

--- auth/index.php
+++
@@ -1,7 +0,0 @@
-<?
-define("NEED_AUTH", true);
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-
-LocalRedirect('/account/personal/');
-?>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
