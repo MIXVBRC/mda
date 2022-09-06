8610e4349d3d12f93c7eb38b1cd227922ded1140
MIXVBRC <mixvbrc@gmail.com>
2022-09-07 05:55:13 +1000

    mda

--- about/feedback.php
+++ about/feedback.php
@@ -1,5 +1,5 @@
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
-$APPLICATION->SetTitle(""); ?><?$APPLICATION->IncludeComponent(
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");?>
+<?$APPLICATION->IncludeComponent(
 	"bitrix:form",
 	"",
 	Array(
