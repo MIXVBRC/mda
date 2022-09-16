9768b81726449ece596a0e11bde01cbe3229af09
MIXVBRC <mixvbrc@gmail.com>
2022-09-16 20:15:24 +1000

    mda

--- local/templates/mda/components/bitrix/sale.personal.order.detail/.default/template.php
+++ local/templates/mda/components/bitrix/sale.personal.order.detail/.default/template.php
@@ -1,5 +1,8 @@
-<?
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
 
 use Bitrix\Main\Localization\Loc,
 	Bitrix\Main\Page\Asset;
@@ -11,30 +14,34 @@ if ($arParams['GUEST_MODE'] !== 'Y')
 }
 $this->addExternalCss("/bitrix/css/main/bootstrap.css");
 
+/**
+ * clipboard - копирование при нажатии на иконку
+ * fx - подгрузка платежных систем в "Сменить способ оплаты"
+ */
 CJSCore::Init(array('clipboard', 'fx'));
 
-$APPLICATION->SetTitle("");
+$APPLICATION->SetTitle(Loc::getMessage('SPOD_LIST_MY_ORDER', [
+    '#ACCOUNT_NUMBER#' => htmlspecialcharsbx($arResult["ACCOUNT_NUMBER"]),
+    '#DATE_ORDER_CREATE#' => $arResult["DATE_INSERT_FORMATED"]
+]));
 
-if (!empty($arResult['ERRORS']['FATAL']))
-{
-	foreach ($arResult['ERRORS']['FATAL'] as $error)
-	{
+
+if (!empty($arResult['ERRORS']['FATAL'])) {
+
+	foreach ($arResult['ERRORS']['FATAL'] as $error) {
 		ShowError($error);
 	}
 
 	$component = $this->__component;
 
-	if ($arParams['AUTH_FORM_IN_TEMPLATE'] && isset($arResult['ERRORS']['FATAL'][$component::E_NOT_AUTHORIZED]))
-	{
+	if ($arParams['AUTH_FORM_IN_TEMPLATE'] && isset($arResult['ERRORS']['FATAL'][$component::E_NOT_AUTHORIZED])) {
 		$APPLICATION->AuthForm('', false, false, 'N', false);
 	}
-}
-else
-{
-	if (!empty($arResult['ERRORS']['NONFATAL']))
-	{
-		foreach ($arResult['ERRORS']['NONFATAL'] as $error)
-		{
+} else {
+
+    /** Ошибки */
+	if (!empty($arResult['ERRORS']['NONFATAL'])) {
+		foreach ($arResult['ERRORS']['NONFATAL'] as $error) {
 			ShowError($error);
 		}
 	}
@@ -45,12 +52,8 @@ else
 
 
         <?/** Заголовок */?>
-        <?
-        $APPLICATION->SetTitle(Loc::getMessage('SPOD_LIST_MY_ORDER', [
-            '#ACCOUNT_NUMBER#' => htmlspecialcharsbx($arResult["ACCOUNT_NUMBER"]),
-            '#DATE_ORDER_CREATE#' => $arResult["DATE_INSERT_FORMATED"]
-        ]));
-        ?>
+        <?// TODO: Вывел выше по коду в метод $APPLICATION->SetTitle() ?>
+        <?/*
 		<div class="sale-order-detail-title-container">
 			<h1 class="sale-order-detail-title-element">
 				<?= Loc::getMessage('SPOD_LIST_MY_ORDER', array(
@@ -59,7 +62,7 @@ else
 				)) ?>
 			</h1>
 		</div>
-
+        */?>
 
         <?/** Навигаторы */?>
 		<? if ($arParams['GUEST_MODE'] !== 'Y'):?>
@@ -130,28 +133,20 @@ else
 									<div class="sale-order-detail-about-order-inner-container-name-title">
 										<?
 										$userName = $arResult["USER_NAME"];
-										if (mb_strlen($userName) || mb_strlen($arResult['FIO']))
-										{
+										if (mb_strlen($userName) || mb_strlen($arResult['FIO'])) {
 											echo Loc::getMessage('SPOD_LIST_FIO').':';
-										}
-										else
-										{
+										} else {
 											echo Loc::getMessage('SPOD_LOGIN').':';
 										}
 										?>
 									</div>
 									<div class="sale-order-detail-about-order-inner-container-name-detail">
 										<?
-										if($userName <> '')
-										{
+										if($userName <> '') {
 											echo htmlspecialcharsbx($userName);
-										}
-										elseif(mb_strlen($arResult['FIO']))
-										{
+										} elseif(mb_strlen($arResult['FIO'])) {
 											echo htmlspecialcharsbx($arResult['FIO']);
-										}
-										else
-										{
+										} else {
 											echo htmlspecialcharsbx($arResult["USER"]['LOGIN']);
 										}
 										?>
@@ -198,7 +193,7 @@ else
 									</div>
 								</div>
 
-                                <?/** Повторить/отменить заказ */?>
+                                <?/** Кнопки повторить и отменить заказ */?>
 								<?if ($arParams['GUEST_MODE'] !== 'Y'):?>
 									<div class="col-md-2 col-sm-6 sale-order-detail-about-order-inner-container-repeat">
 
@@ -300,6 +295,7 @@ else
 											</li>
                                         <?endforeach;?>
 									<?endif;?>
+
 								</ul>
 
                                 <?/** Комментарии к заказу */?>
@@ -312,6 +308,15 @@ else
 										<?= nl2br(htmlspecialcharsbx($arResult["USER_DESCRIPTION"])) ?>
 									</div>
                                 <?endif;?>
+
+                                <!-- ТЕСТ -->
+                                <h4 class="sale-order-detail-about-order-inner-container-details-title sale-order-detail-about-order-inner-container-comments">
+                                    Комментарии к заказу
+                                </h4>
+                                <div
+                                        class="col-xs-12 sale-order-detail-about-order-inner-container-list-item-element">
+                                    <?= nl2br(htmlspecialcharsbx('Тестовый комментарий')) ?>
+                                </div>
 
 							</div>
 
@@ -673,6 +678,14 @@ else
 																	</div>
                                                                 <?endif;?>
 
+                                                                <!-- ТЕСТ -->
+                                                                <div class="sale-order-detail-payment-options-methods-shipment-list-item">
+                                                                    <span class="sale-order-list-shipment-id-name">Идентификатор отправления:</span>
+                                                                    <?// при клике на иконку копируется номер?>
+                                                                    <span class="sale-order-detail-shipment-id">1234567890<?= htmlspecialcharsbx($shipment['TRACKING_NUMBER']) ?></span>
+                                                                    <span class="sale-order-detail-shipment-id-icon"></span>
+                                                                </div>
+
                                                                 <?/** Показать все / Свернуть */?>
 																<div class="sale-order-detail-payment-options-methods-shipment-list-item-link">
 																	<a class="sale-order-detail-show-link"><?= Loc::getMessage('SPOD_LIST_SHOW_ALL')?></a>
@@ -689,6 +702,14 @@ else
 																	</a>
 																</div>
                                                             <?endif;?>
+
+                                                            <!-- ТЕСТ -->
+                                                            <div class="col-md-2 col-sm-12 sale-order-detail-payment-options-shipment-button-container">
+                                                                <a class="pre sale-order-detail-payment-options-shipment-button-element"
+                                                                   href="<?= $shipment['TRACKING_URL'] ?>">
+                                                                    Проверить отправление
+                                                                </a>
+                                                            </div>
 
 														</div><!--row-->
 
@@ -1267,7 +1288,6 @@ else
 	<script>
 		BX.Sale.PersonalOrderComponent.PersonalOrderDetail.init(<?=$javascriptParams?>);
 	</script>
-
 
 <?
 }
