a29ffbb1e226ad9eaa330d9890177b74af098b73
MIXVBRC <mixvbrc@gmail.com>
2022-09-30 01:56:12 +1000

    mda

--- local/templates/mda/components/bitrix/catalog/.default/bitrix/catalog.section.list/.default/template.php
+++ local/templates/mda/components/bitrix/catalog/.default/bitrix/catalog.section.list/.default/template.php
@@ -16,28 +16,27 @@ $strSectionEdit = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "SECTION_EDIT");
 $strSectionDelete = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "SECTION_DELETE");
 $arSectionDeleteParams = array("CONFIRM" => GetMessage('CT_BCSL_ELEMENT_DELETE_CONFIRM'));
 
-if ($arResult["SECTIONS_COUNT"] > 0):?>
+if ($arResult["SECTIONS_COUNT"] > 0 && $arResult['SECTIONS']):?>
 
-<div class="sections-mini">
-    <div class="sections-mini__body">
-        <div class="sections-mini__list">
+    <div class="sections-mini">
+        <div class="sections-mini__body">
+            <div class="sections-mini__list">
 
-            <?foreach ($arResult['SECTIONS'] as $arSection):?>
-                <?
-                if ($arSection['ELEMENT_CNT'] <= 0) continue;
+                <?foreach ($arResult['SECTIONS'] as $arSection):?>
 
-                $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
-                $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
-                ?>
+                    <?
+                    $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], $strSectionEdit);
+                    $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], $strSectionDelete, $arSectionDeleteParams);
+                    ?>
 
-                <div class="sections-mini__item" id="<?=$this->GetEditAreaId($arSection['ID'])?>">
-                    <a href="<?=$arSection['SECTION_PAGE_URL']?>" class="sections-mini__link"><?=$arSection['NAME']?></a>
-                </div>
+                    <div class="sections-mini__item" id="<?=$this->GetEditAreaId($arSection['ID'])?>">
+                        <a href="<?=$arSection['SECTION_PAGE_URL']?>" class="sections-mini__link"><?=$arSection['NAME']?></a>
+                    </div>
 
-            <?endforeach;?>
+                <?endforeach;?>
 
+            </div>
         </div>
     </div>
-</div>
 
 <?endif;?>
